06471ce185c581bca553bc3d5394b082c9c33346
justarunsthing <justarunsthing@outlook.com>
2025-12-31 21:13:35 +0000

    initial commit

---
+++ BlazorWebAppWithAuth/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace BlazorWebAppWithAuth.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ BlazorWebAppWithAuth/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace BlazorWebAppWithAuth.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
