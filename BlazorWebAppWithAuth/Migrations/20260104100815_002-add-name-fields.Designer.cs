954acb4c7b71f245032ca2a77b9a36b4f300d434
justarunsthing <justarunsthing@outlook.com>
2026-01-04 10:09:08 +0000

    Added migration for name fields

--- BlazorWebAppWithAuth/Migrations/20260104100815_002-add-name-fields.Designer.cs
+++ BlazorWebAppWithAuth/Migrations/20260104100815_002-add-name-fields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlazorWebAppWithAuth.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlazorWebAppWithAuth.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260104100815_002-add-name-fields")]
+    partial class _002addnamefields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
