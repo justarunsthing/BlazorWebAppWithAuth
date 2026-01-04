cf0e388c3355294c95e1ccddb82adf672629523f
justarunsthing <justarunsthing@outlook.com>
2026-01-04 10:02:46 +0000

    Added name properteis to ApplicationUser

--- BlazorWebAppWithAuth/Data/ApplicationUser.cs
+++ BlazorWebAppWithAuth/Data/ApplicationUser.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorWebAppWithAuth.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-    }
+        [Required]
+        public string? FirstName { get; set; }
 
+        [Required]
+        public string? LastName { get; set; }
+    }
 }
