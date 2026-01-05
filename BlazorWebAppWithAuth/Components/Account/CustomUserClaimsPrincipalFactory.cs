26e11ddedc95a0d5e3f22f231a814ef29fc772ac
justarunsthing <justarunsthing@outlook.com>
2026-01-05 08:18:36 +0000

    Added CustomUserClaimsPrincipalFactory class

---
+++ BlazorWebAppWithAuth/Components/Account/CustomUserClaimsPrincipalFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using BlazorWebAppWithAuth.Data;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorWebAppWithAuth.Components.Account
+{
+    public class CustomUserClaimsPrincipalFactory(UserManager<ApplicationUser> userManager, IOptions<IdentityOptions> options)
+        : UserClaimsPrincipalFactory<ApplicationUser>(userManager, options)
+    {
+
+    }
+}
