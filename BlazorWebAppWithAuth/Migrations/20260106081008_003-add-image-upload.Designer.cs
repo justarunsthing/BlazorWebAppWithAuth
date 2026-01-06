53d2c823093e6aa8d8a76e078211571fe6482833
justarunsthing <justarunsthing@outlook.com>
2026-01-06 08:10:49 +0000

    Added migration for image upload

--- BlazorWebAppWithAuth/Migrations/20260106081008_003-add-image-upload.Designer.cs
+++ BlazorWebAppWithAuth/Migrations/20260106081008_003-add-image-upload.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlazorWebAppWithAuth.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlazorWebAppWithAuth.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260106081008_003-add-image-upload")]
+    partial class _003addimageupload
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
