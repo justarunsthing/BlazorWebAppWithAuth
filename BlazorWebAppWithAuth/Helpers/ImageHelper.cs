b2fc24f14c6eebf6fb0063fa04753ee11ebae8b4
justarunsthing <justarunsthing@outlook.com>
2026-01-07 08:25:20 +0000

    Added ImageHelper class

---
+++ BlazorWebAppWithAuth/Helpers/ImageHelper.cs
@@ -0,0 +1,30 @@
+﻿using BlazorWebAppWithAuth.Models;
+
+namespace BlazorWebAppWithAuth.Helpers
+{
+    public static class ImageHelper
+    {
+        private static readonly string DefaultProfilePictureUrl = "/images/undraw_person.svg";
+
+        public static async Task<ImageUpload> GetImageUploadAsync(IFormFile file)
+        {
+            using var ms = new MemoryStream();
+            await file.CopyToAsync(ms);
+            byte[] data = ms.ToArray();
+
+            if (ms.Length > 1 * 1024 * 1024)
+            {
+                throw new Exception("The image size cannot exceed 1 MB.");
+            }
+
+            var imageUpload = new ImageUpload
+            {
+                Id = Guid.NewGuid(),
+                Data = data,
+                Type = file.ContentType
+            };
+
+            return imageUpload;
+        }
+    }
+}
