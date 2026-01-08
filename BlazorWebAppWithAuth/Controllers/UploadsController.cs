2e0cbcab13ea969db24585a810c86f20ace16340
justarunsthing <justarunsthing@outlook.com>
2026-01-08 07:52:28 +0000

    Added UploadsController

---
+++ BlazorWebAppWithAuth/Controllers/UploadsController.cs
@@ -0,0 +1,12 @@
+﻿using BlazorWebAppWithAuth.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWebAppWithAuth.Controllers
+{
+    [Route("uploads")]
+    [ApiController]
+    public class UploadsController(ApplicationDbContext context) : ControllerBase
+    {
+    }
+}
