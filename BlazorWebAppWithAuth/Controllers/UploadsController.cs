171c08f427774e3b30f7086296d335a42f6b8c6e
justarunsthing <justarunsthing@outlook.com>
2026-01-08 07:57:28 +0000

    Added GetImageAsync method

--- BlazorWebAppWithAuth/Controllers/UploadsController.cs
+++ BlazorWebAppWithAuth/Controllers/UploadsController.cs
@@ -1,6 +1,7 @@
-﻿using BlazorWebAppWithAuth.Data;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using BlazorWebAppWithAuth.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OutputCaching;
 
 namespace BlazorWebAppWithAuth.Controllers
 {
@@ -8,5 +9,18 @@ namespace BlazorWebAppWithAuth.Controllers
     [ApiController]
     public class UploadsController(ApplicationDbContext context) : ControllerBase
     {
+        [HttpGet("{id:guid}")]
+        [OutputCache(VaryByRouteValueNames = ["id"], Duration = 60 * 60 * 24)]
+        public async Task<IActionResult> GetImageAsync(Guid id)
+        {
+            var image = await context.Images.FirstOrDefaultAsync(i => i.Id == id);
+
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return File(image.Data!, image.Type!);
+        }
     }
 }
